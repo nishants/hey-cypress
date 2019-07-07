426e72b1d119e6670822bc60766c1720a5eeef60
nishant <nishant.singh87@gmail.com>
2019-07-07 14:43:01 +0530

    Select element and assert.

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,23 @@
+describe('MyFirst test', () => {
+  beforeEach(() => {
+    cy.visit('http://blazedemo.com/');
+  });
+  it('lest open a url', () => {
+    cy.contains('destination of the week! The Beach!').click();
+    cy.url().should('include', 'vacation.html');
+    cy.get('.img-rounded').invoke('attr', 'src').should('include', 'assets/vacation.jpg');
+    cy.contains('Destination of the week: Hawaii !').should('exist');
+    cy.get('.brand').first().click();
+    cy.get('[name="fromPort"]').select('Mexico City');
+    cy.get('[name="toPort"]').select('New York');
+    cy.get('input[value="Find Flights"]').click();
+    cy.contains('Flights from Mexico City to New York:').should('exist');
+    cy.get('td').contains('Lufthansa').parent().find('input[type="submit"]').as("bookThisFlight");
+    cy.get("@bookThisFlight").invoke('attr', 'value').should('include', 'Choose This Flight');
+    cy.get("@bookThisFlight").click();
+  });
+
+  it.only('should intercept and modify api data', () => {
+
+  });
+});
